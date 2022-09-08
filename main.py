9d2af7ad26d6130e56b59b304e97e83d793f774e
Arthur Neres <106874961+artxpng@users.noreply.github.com>
2022-09-08 14:22:36 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 #coding utf-8
 
-def 
+from titularpf import titularPF
+
+if __name__ == '__main__':
+    arthur = titularPF('Arthur', 20, '123456', '(75) 8888-9999')
+
+    saque = float(input("..."))
+    arthur.getConta().sacar(saque)
